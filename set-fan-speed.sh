96434282b5803a7b96cab192608d9de967e31285
Nathan Key <nate13key@gmail.com>
2026-09-14 21:50:27 -0500

    Get flags for input instead of hardcoding

--- set-fan-speed.sh
+++ set-fan-speed.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# $OPTION is hardcoded for now
-OPTION=100
 
-#Checks to make sure .env file exists, then imports the variables. 
+# Checks to make sure .env file exists, then imports the variables. 
 ENV_FILE="$(dirname "$0")/.env"
 if [ ! -f "$ENV_FILE" ]; then
 	echo "Missing .env file at $ENV_FILE" >&2
@@ -13,22 +11,49 @@ set -a
 source "$ENV_FILE"
 set +a
 
-# Main
+# Get $OPTION
+usage() {
+        echo "Usage: $0 [-p | -m | -s | -a]"
+        echo "  -p  Disable third-party PCIe response"
+        echo "  -m  Enable manual fan management"
+        echo "  -s  Set fans to slow (20%)"
+        echo "  -a  Revert to automatic fan management"
+        exit 1
+}
+
+while getopts "pmsa" opt; do
+        if [ -n "$OPTION" ]; then
+                echo "Error: only one flag may be specified at a time" >&2
+                usage
+        fi
+        case "$opt" in
+                p) OPTION=1 ;;
+                m) OPTION=2 ;;
+                s) OPTION=3 ;;
+                a) OPTION=4 ;;
+                *) usage ;;
+        esac
+done
+
+if [ -z "$OPTION" ]; then
+        usage
+fi
+
 if [ "$OPTION" -eq 1 ]; then
 	echo "Option $OPTION"
-	# Disables third party PCIe response
+	echo "Disabling third party PCIe response"
 	ipmitool -I lanplus -H "$IP" -U "$USERNAME" -P "$PASSWORD" raw 0x30 0xce 0x00 0x16 0x5 0x00 0x00 0x00 0x05 0x00 0x01 0x00 0x00
 elif [ "$OPTION" -eq 2 ]; then
 	echo "Option $OPTION"
-	# Tells iDRAC to stop managing the fans
+	echo "Enabling manual fan management"
 	ipmitool -I lanplus -H "$IP" -U "$USERNAME" -P "$PASSWORD" raw 0x30 0x30 0x01 0x00
 elif [ "$OPTION" -eq 3 ]; then
 	echo "Option $OPTION"
-	# Sets all fans to 20%
+	echo "Setting all fans to 20%"
 	ipmitool -I lanplus -H "$IP" -U "$USERNAME" -P "$PASSWORD" raw 0x30 0x30 0x02 0xff 0x14
 elif [ "$OPTION" -eq 4 ]; then
 	echo "Option $OPTION"
-	# Reverts control of fan speed to automatic
+	echo "Reverting control of fan speed to automatic"
 	ipmitool -I lanplus -H "$IP" -U "$USERNAME" -P "$PASSWORD" raw 0x30 0x30 0x01 0x01
 else
 	echo "Option not recognized"
